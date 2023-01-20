11c7d911dc2fa6e1430503923528d20b4df6590d
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2023-01-20 18:24:37 +0100

    Initial commit

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+
+const apiKey = '1zzfx3rP6MkaivhkbblHD3Kzg70ad4yLN563rKtbi';
+const appId = 'com.gmail.luca.tonelli1999.microverse';
+export default  {apiKey, appId};
---
+++ behaviors/default/sendPos.js
@@ -0,0 +1,36 @@
+class SendPos {
+    setup() {
+        this.addEventListener("pointerDown", "send");
+        this.pos = this._translation;
+        this.spinning = false; // start without spinning
+        this.angle = 0; // the initial angle
+        this.spinSpeed = 0.01; // how fast will we spin (in radians)
+       
+    }
+    step() {
+        if (!this.spinning) return;
+        this.future(20).step();
+        this.angle += this.spinSpeed;
+        this.set({ rotation: Microverse.q_euler(0, this.angle, 0) });
+    }
+    send() {
+        this.spinning = !this.spinning;
+        if (this.spinning) this.step();
+        publish("obj1", "pos", [1,1,1]);
+    }
+    teardown() {
+        this.removeEventListener("pointerDown", "toggle");
+        this.spinning = undefined;
+    }
+
+
+}
+
+export default {
+    modules: [
+        {
+            name: "sendPos",
+            pawnBehaviors: [SendPos],
+        }
+    ]
+}
